50a12befa982c9b9dc96804df6ed54cb321b89d9
Mushi42 <musharaf42@gmail.com>
2022-05-01 01:49:41 +0500

    add status in trans mode

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -19,6 +19,7 @@ const transactionSchema = new mongoose.Schema({
     transactionType: {
         type: String
     },
+    status: String,
     createdAt: {
         type: Date,
         default: new Date(),
